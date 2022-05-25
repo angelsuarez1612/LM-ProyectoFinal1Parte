1439ac671c404d67a57b668ff992597ab92b46de
angelsuarez1612 <98739735+angelsuarez1612@users.noreply.github.com>
2022-05-25 19:38:11 +0200

    Introduce moneda base,obejtivo,fecha y devuelve valores
    Introduce moneda base, otra moneda a la que vamos a convertir esa moneda base y una fecha en la que queramos saber cuanto era el valor de la moneda base en la otra moneda

---
+++ prog3.py
@@ -0,0 +1,20 @@
+import requests
+import json
+import os
+
+url_base="https://exchange-rates.abstractapi.com/v1/"
+key="bd37a8482ef1452bb4a63e59900b08ae"
+moneda1=input("Moneda a buscar: ")
+moneda2=input("Moneda a devolver: ")
+fecha=input("Introduce la fecha en la que buscar: (FORMATO YYY-MM-DD) ")
+
+payload={'api_key':key,"base":moneda1,"target":moneda2,"date":fecha}
+
+url=url_base+"historical"
+r=requests.get(url,params=payload)
+if r.status_code == 200:
+    datos=r.json()
+    print(json.dumps(datos,indent=4,sort_keys=True))
+else:
+    print("Error")
+    print(r.status_code)
